d7c1d434f1dac102fae3f3290defd5ae223c7e6d
manticore563 <onlineexams786@gmail.com>
2025-03-02 21:03:40 +0530

    push

--- install/install.php
+++ install/install.php
@@ -1,11 +1,19 @@
 <?php
+// Check if already installed
 if (file_exists('../.env')) {
     header('Location: ../public/');
     exit;
 }
-require '../system/bootstrap.php';
+
+// Bootstrap CodeIgniter
+define('FCPATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+require_once '../system/bootstrap.php';
+
+// Manual routing for installer
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$uri = str_replace('/exambook/install', '', $uri) ?: '/';
 $router = service('router');
 $router->setDefaultNamespace('App\Controllers');
 $router->setDefaultController('InstallController');
+$router->setDefaultMethod('index');
 $router->handle($uri);
